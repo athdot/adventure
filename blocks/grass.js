87286ff227edeb5710bb735d98f18a87c5d2ec21
athdot <athdot@gmail.com>
2017-05-24 11:05:06 -0600

    Create grass.js

---
+++ blocks/grass.js
@@ -0,0 +1,57 @@
+ //I make dirt texture =)
+            var randirt = 1;
+    while(randirt < 6){
+           	 //edit dirt image
+             var dirtimg = document.createElement("img");
+             dirtimg.style = "display:none;"
+             dirtimg.id = "grass"+randirt;
+             document.body.appendChild(dirtimg);
+         var dirty = document.createElement("canvas");
+		 dirty.style = "display:none;"
+		 dirty.width = "64";
+		 dirty.height = "64";
+		 document.body.appendChild(dirty);
+		 {
+			 var dirtyctx = dirty.getContext("2d");
+    		 var xdirt = 0;
+    		 var ydirt = 0;
+    		 while(xdirt < dirty.width){
+         var grassheight = Math.floor((Math.random() * 16) + 5);
+    			 while(ydirt < dirty.height){
+           if(grassheight > ydirt){
+        			 var randint = Math.floor((Math.random() * 3) + 1);
+           			 if(randint == 1){
+           			      dirtyctx.fillStyle = "rgba(43,171,73,1)";
+              			  dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 2){
+                			dirtyctx.fillStyle = "rgba(38,151,64,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 3){
+                			dirtyctx.fillStyle = "rgba(51,138,71,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}
+           				var ydirt = ydirt+1;           
+           }else{
+        			 var randint = Math.floor((Math.random() * 3) + 1);
+           			 if(randint == 1){
+           			      dirtyctx.fillStyle = "rgba(97,65,78,1)";
+              			  dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 2){
+                			dirtyctx.fillStyle = "rgba(112,84,59,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 3){
+                			dirtyctx.fillStyle = "rgba(140,118,98,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}
+           				var ydirt = ydirt+1;
+        			}
+              }
+        			var ydirt = 0;
+    				var xdirt = xdirt+1;
+    			}
+			}
+			var dataurl = dirty.toDataURL();
+            document.getElementById("grass"+randirt).src = dataurl;
+var randirt = randirt+1;
+        }
+        //Yay! made dirt texture!
