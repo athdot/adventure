7b4bc353b580a66c75717db4129734467bf16664
athdot <athdot@gmail.com>
2017-05-25 09:58:24 -0600

    Update grass-slope-left.js

--- blocks/grass-slope-left.js
+++ blocks/grass-slope-left.js
@@ -1,51 +1,51 @@
-var grass = document.createElement("canvas");
-grass.width = "64";
-grass.height = "64";
-grass.style = "display:none;";
-document.body.appendChild(grass);
-var grassctx = grass.getContext("2d");
-var ygrass = 64;
-var xgrass = 0;
-while(xgrass < grass.width){
-var distbottom = 64-ygrass;
-var grassheight = Math.floor((Math.random() * 11) + 5);
-while(ygrass < grass.height){
-var dist2 = 64-ygrass;
-var randchange = Math.floor((Math.random() * 5) - 5);
-if(distbottom < dist2+grassheight){
-var randint = Math.floor((Math.random() * 3) + 1);
-if(randint == 1){
-grassctx.fillStyle = "rgba(43,171,73,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
+var grasss = document.createElement("canvas");
+grasss.width = "64";
+grasss.height = "64";
+grasss.style = "display:none;";
+document.body.appendChild(grasss);
+var grasssctx = grasss.getContext("2d");
+var ygrasss = 64;
+var xgrasss = 0;
+while(xgrasss < grasss.width){
+var distbottoms = 64-ygrasss;
+var grasssheight = Math.floor((Math.random() * 11) + 5);
+while(ygrasss < grasss.height){
+var dist2s = 64-ygrasss;
+var randchanges = Math.floor((Math.random() * 5) - 5);
+if(distbottoms < dist2s+grasssheight){
+var randints = Math.floor((Math.random() * 3) + 1);
+if(randints == 1){
+grasssctx.fillStyle = "rgba(43,171,73,1)";
+grassctx.fillRect( xgrasss, ygrasss, 1, 1 );
 }else if(randint == 2){
-grassctx.fillStyle = "rgba(38,151,64,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
+grasssctx.fillStyle = "rgba(38,151,64,1)";
+grasssctx.fillRect( xgrasss, ygrasss, 1, 1 );
 }else if(randint == 3){
-grassctx.fillStyle = "rgba(51,138,71,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
+grasssctx.fillStyle = "rgba(51,138,71,1)";
+grasssctx.fillRect( xgrasss, ygrasss, 1, 1 );
 }
 }else{
 
-var randint = Math.floor((Math.random() * 3) + 1);
-if(randint == 1){
-grassctx.fillStyle = "rgba(97,65,78,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
-}else if(randint == 2){
-grassctx.fillStyle = "rgba(112,84,59,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
-}else if(randint == 3){
-grassctx.fillStyle = "rgba(140,118,98,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
+var randints = Math.floor((Math.random() * 3) + 1);
+if(randints == 1){
+grasssctx.fillStyle = "rgba(97,65,78,1)";
+grasssctx.fillRect( xgrasss, ygrasss, 1, 1 );
+}else if(randints == 2){
+grasssctx.fillStyle = "rgba(112,84,59,1)";
+grasssctx.fillRect( xgrasss, ygrasss, 1, 1 );
+}else if(randints == 3){
+grasssctx.fillStyle = "rgba(140,118,98,1)";
+grasssctx.fillRect( xgrasss, ygrasss, 1, 1 );
 }
 
 }
-var ygrass = ygrass + 1;
+var ygrasss = ygrasss + 1;
 }
-var ygrass = 64-xgrass+randchange;
-var xgrass = xgrass + 1;
+var ygrasss = 64-xgrasss+randchange;
+var xgrasss = xgrasss + 1;
 }
 var dataurl = grass.toDataURL();
 var imag = document.createElement("img");
 imag.src = dataurl;
-imag.id = "grassright"
+imag.id = "grassleft"
 document.body.appendChild(imag);
