77a773aaa3e9a89b9535f29d54141273df615525
athdot <athdot@gmail.com>
2017-05-25 15:40:40 -0600

    Update grass-slope-left.js

--- blocks/grass-slope-left.js
+++ blocks/grass-slope-left.js
@@ -1,51 +1,57 @@
-var grasss = document.createElement("canvas");
-grasss.width = "64";
-grasss.height = "64";
-grasss.style = "display:none;";
-document.body.appendChild(grasss);
-var grasssctx = grasss.getContext("2d");
-var ygrasss = 64;
-var xgrasss = 0;
-while(xgrasss < grasss.width){
-var distbottoms = 64-ygrasss;
-var grasssheight = Math.floor((Math.random() * 11) + 5);
-while(ygrasss < grasss.height){
-var dist2s = 64-ygrasss;
-var randchanges = Math.floor((Math.random() * 5) - 5);
-if(distbottoms < dist2s+grasssheight){
-var randints = Math.floor((Math.random() * 3) + 1);
-if(randints == 1){
-grasssctx.fillStyle = "rgba(43,171,73,1)";
-grassctx.fillRect( xgrasss, ygrasss, 1, 1 );
-}else if(randint == 2){
-grasssctx.fillStyle = "rgba(38,151,64,1)";
-grasssctx.fillRect( xgrasss, ygrasss, 1, 1 );
-}else if(randint == 3){
-grasssctx.fillStyle = "rgba(51,138,71,1)";
-grasssctx.fillRect( xgrasss, ygrasss, 1, 1 );
-}
-}else{
-
-var randints = Math.floor((Math.random() * 3) + 1);
-if(randints == 1){
-grasssctx.fillStyle = "rgba(97,65,78,1)";
-grasssctx.fillRect( xgrasss, ygrasss, 1, 1 );
-}else if(randints == 2){
-grasssctx.fillStyle = "rgba(112,84,59,1)";
-grasssctx.fillRect( xgrasss, ygrasss, 1, 1 );
-}else if(randints == 3){
-grasssctx.fillStyle = "rgba(140,118,98,1)";
-grasssctx.fillRect( xgrasss, ygrasss, 1, 1 );
-}
-
-}
-var ygrasss = ygrasss + 1;
-}
-var ygrasss = 64-xgrasss+randchanges;
-var xgrasss = xgrasss + 1;
-}
-var dataurls = grasss.toDataURL();
-var imagea = document.createElement("img");
-imagea.src = dataurls;
-imagea.id = "grassleft"
-document.body.appendChild(imagea);
+ //I make dirt texture =)
+            var randirt = 1;
+    while(randirt < 6){
+           	 //edit dirt image
+             var dirtimg = document.createElement("img");
+             dirtimg.style = "display:none;"
+             dirtimg.id = "grassleft"+randirt;
+             document.body.appendChild(dirtimg);
+         var dirty = document.createElement("canvas");
+		 dirty.style = "display:none;"
+		 dirty.width = "64";
+		 dirty.height = "64";
+		 document.body.appendChild(dirty);
+		 {
+			 var dirtyctx = dirty.getContext("2d");
+    		 var xdirt = 0;
+    		 var ydirt = 64;
+    		 while(xdirt < dirty.width){
+         var grassheight = Math.floor((Math.random() * 16) + 5);
+    			 while(ydirt < dirty.height){
+           if(grassheight > ydirt){
+        			 var randint = Math.floor((Math.random() * 3) + 1);
+           			 if(randint == 1){
+           			      dirtyctx.fillStyle = "rgba(43,171,73,1)";
+              			  dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 2){
+                			dirtyctx.fillStyle = "rgba(38,151,64,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 3){
+                			dirtyctx.fillStyle = "rgba(51,138,71,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}
+           				var ydirt = ydirt+1;           
+           }else{
+        			 var randint = Math.floor((Math.random() * 3) + 1);
+           			 if(randint == 1){
+           			      dirtyctx.fillStyle = "rgba(97,65,78,1)";
+              			  dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 2){
+                			dirtyctx.fillStyle = "rgba(112,84,59,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 3){
+                			dirtyctx.fillStyle = "rgba(140,118,98,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}
+           				var ydirt = ydirt+1;
+        			}
+              }
+        			var ydirt = 64-xdirt;
+    				var xdirt = xdirt+1;
+    			}
+			}
+			var dataurl = dirty.toDataURL();
+            document.getElementById("grassleft"+randirt).src = dataurl;
+var randirt = randirt+1;
+        }
+        //Yay! made dirt texture!
