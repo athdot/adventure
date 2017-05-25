d829c4d185476494f9ad0f461a77bd2ce7e5b39b
athdot <athdot@gmail.com>
2017-05-25 15:41:59 -0600

    Update grass-slope-right.js

--- blocks/grass-slope-right.js
+++ blocks/grass-slope-right.js
@@ -1,51 +1,57 @@
-var grass = document.createElement("canvas");
-grass.width = "64";
-grass.height = "64";
-grass.style = "display:none;";
-document.body.appendChild(grass);
-var grassctx = grass.getContext("2d");
-var ygrass = 0;
-var xgrass = 0;
-while(xgrass < grass.width){
-var distbottom = 64-ygrass;
-var grassheight = Math.floor((Math.random() * 11) + 5);
-while(ygrass < grass.height){
-var dist2 = 64-ygrass;
-var randchange = Math.floor((Math.random() * 5) - 5);
-if(distbottom < dist2+grassheight){
-var randint = Math.floor((Math.random() * 3) + 1);
-if(randint == 1){
-grassctx.fillStyle = "rgba(43,171,73,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
-}else if(randint == 2){
-grassctx.fillStyle = "rgba(38,151,64,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
-}else if(randint == 3){
-grassctx.fillStyle = "rgba(51,138,71,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
-}
-}else{
-
-var randint = Math.floor((Math.random() * 3) + 1);
-if(randint == 1){
-grassctx.fillStyle = "rgba(97,65,78,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
-}else if(randint == 2){
-grassctx.fillStyle = "rgba(112,84,59,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
-}else if(randint == 3){
-grassctx.fillStyle = "rgba(140,118,98,1)";
-grassctx.fillRect( xgrass, ygrass, 1, 1 );
-}
-
-}
-var ygrass = ygrass + 1;
-}
-var ygrass = xgrass+randchange;
-var xgrass = xgrass + 1;
-}
-var dataurl = grass.toDataURL();
-var imag = document.createElement("img");
-imag.src = dataurl;
-imag.id = "grassright"
-document.body.appendChild(imag);
+ //I make dirt texture =)
+            var randirt = 1;
+    while(randirt < 6){
+           	 //edit dirt image
+             var dirtimg = document.createElement("img");
+             dirtimg.id = "grassright"+randirt;
+             dirtimg.style = "display:none;"
+             document.body.appendChild(dirtimg);
+         var dirty = document.createElement("canvas");
+		 dirty.style = "display:none;"
+		 dirty.width = "64";
+		 dirty.height = "64";
+		 document.body.appendChild(dirty);
+		 {
+			 var dirtyctx = dirty.getContext("2d");
+    		 var xdirt = 0;
+    		 var ydirt = 0;
+    		 while(xdirt < dirty.width){
+         var grassheight = Math.floor((Math.random() * 16) + 5);
+    			 while(ydirt < dirty.height){
+           if(grassheight > ydirt){
+        			 var randint = Math.floor((Math.random() * 3) + 1);
+           			 if(randint == 1){
+           			      dirtyctx.fillStyle = "rgba(43,171,73,1)";
+              			  dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 2){
+                			dirtyctx.fillStyle = "rgba(38,151,64,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 3){
+                			dirtyctx.fillStyle = "rgba(51,138,71,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}
+           				var ydirt = ydirt+1;           
+           }else{
+        			 var randint = Math.floor((Math.random() * 3) + 1);
+           			 if(randint == 1){
+           			      dirtyctx.fillStyle = "rgba(97,65,78,1)";
+              			  dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 2){
+                			dirtyctx.fillStyle = "rgba(112,84,59,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}else if(randint == 3){
+                			dirtyctx.fillStyle = "rgba(140,118,98,1)";
+                			dirtyctx.fillRect( xdirt, ydirt, 1, 1 );
+            			}
+           				var ydirt = ydirt+1;
+        			}
+              }
+        			var ydirt = xdirt;
+    				var xdirt = xdirt+1;
+    			}
+			}
+			var dataurl = dirty.toDataURL();
+            document.getElementById("grassright"+randirt).src = dataurl;
+var randirt = randirt+1;
+        }
+        //Yay! made dirt texture!
